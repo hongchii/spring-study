5d10320ec80197546e57afd8e144a7c9b377c635
hongchii <flyh0321@gmail.com>
2022-01-02 00:24:15 +0900

    RequestParam과 ModelAttribute - (2)

---
+++ ch2/src/main/java/com/fastcampus/ch2/YoilTellerMVC6.java
@@ -0,0 +1,71 @@
+package com.fastcampus.ch2;
+
+import java.util.Calendar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class YoilTellerMVC6 {
+
+	@ExceptionHandler(Exception.class)
+	public String catcher(Exception ex, BindingResult result) {
+		System.out.println("result = " + result);
+		FieldError error = result.getFieldError();
+		
+		System.out.println("code="+error.getCode());
+		System.out.println("field="+error.getField());
+		System.out.println("msg="+error.getDefaultMessage());
+		ex.printStackTrace();
+		return "yoilError";
+	}
+
+	@RequestMapping("/getYoilMVC6") // http://localhost/ch2/getYoilMVC6
+//  public String main(@ModelAttribute("myDate") MyDate date, Model model){	// 아래와 동일
+	public String main(MyDate date, BindingResult result) {
+		System.out.println("result = " + result);
+		// 2. 유효성 검사
+		if (!isValid(date)) {
+			return "yoilError";
+		}
+
+		// 3. 처리
+//		char yoil = getYoil(date);
+
+		// 4.
+		// mv.addObject("year", date.getYear());
+		// mv.addObject("month", date.getMonth());
+		// mv.addObject("day", date.getDay());
+//		model.addAttribute("myDate", date);
+//		model.addAttribute("yoil", yoil);
+
+		return "yoil";
+	}
+
+	private char getYoil(MyDate date) {
+		return getYoil(date.getYear(), date.getMonth(), date.getDay());
+	}
+
+	private @ModelAttribute("yoil") char getYoil(int year, int month, int day) {
+		Calendar cal = Calendar.getInstance();
+		cal.set(year, month - 1, day);
+
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+		return " 일월화수목금토".charAt(dayOfWeek);
+	}
+
+	private boolean isValid(MyDate date) {
+		return isValid(date.getYear(), date.getMonth(), date.getDay());
+	}
+
+	private boolean isValid(int year, int month, int day) {
+		if (year == -1 || month == -1 || day == -1)
+			return false;
+
+		return (1 <= month && month <= 12) && (1 <= day && day <= 31); // 간단히 체크
+	}
+}
